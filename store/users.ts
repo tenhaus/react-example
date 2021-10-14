ccbb626668f3d61823360a8d3bc56f96eb6917ad
chrishart0 <chris@chris.chris>
2021-10-14 13:29:16 -0400

    handle user id in url

--- store/users.ts
+++ store/users.ts
@@ -1,13 +1,15 @@
-import { atom } from 'recoil';
+import { atom } from 'jotai';
 import { Profile } from './profile';
 
 export interface UserState {
     id: Number;
+    test: Number;
     profile?: Profile
 }
 
 const defaultUserList: UserState[] = [
     {
+        test: 1,
         id: 1,
         profile: {
             firstName: "Chris",
@@ -18,15 +20,17 @@ const defaultUserList: UserState[] = [
         }
     },
     {
+        
+        test: 1,
         id: 2,
         profile: {
             firstName: "Chris",
             lastName: "Hayen",
             phone: "555-555-5555",
-            email: "chart@contino.io",
-            username: "chart"
+            email: "chayen@contino.io",
+            username: "chayen"
         }
     }
 ];
 
-export const userListAtom = atom({ key: 'userList', default: defaultUserList });
+export const userListAtom = atom(defaultUserList);
